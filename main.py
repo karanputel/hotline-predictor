1e60b1e14e9a03232389fd10557d0eaf7e075867
karanputel <putelk995@gmail.com>
2025-06-02 12:57:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from telegram.ext import (
 )
 from telegram.ext import AIORateLimiter
 
-BOT_TOKEN = "7873773362:AAHSy-N-OujHOB4dNjTuZTzqqgn7A804YAw"
+BOT_TOKEN = "8118873997:AAGPgpQQ6owWq6VK8J0uYKIl53KyGkGHL5w"
 APP_URL = "https://hotline-predictor.onrender.com"  # 🔁 Replace this with your actual Render URL
 
 app = FastAPI()
